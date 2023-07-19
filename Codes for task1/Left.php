2c159ecc17cd1266726dd85e7ac5a8516ff3417e
Emtenan-A <139411172+Emtenan-A@users.noreply.github.com>
2023-07-20 01:00:14 +0300

    Create Left.php

---
+++ Codes for task1/Left.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "robot";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Prepare and bind
+$stmt = $conn->prepare("INSERT INTO task1 (Direction) VALUES (?)");
+$stmt->bind_param("s", $direction);
+
+// Set parameters and execute
+$direction = "Left";
+$stmt->execute();
+
+echo "Left";
+
+$stmt->close();
+$conn->close();
+?>
